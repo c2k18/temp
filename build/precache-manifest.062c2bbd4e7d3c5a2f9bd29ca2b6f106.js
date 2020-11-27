89a3e91d67000de8da9096f489aa632263dfc004
HeptaDecane <sooraj1999vs@gmail.com>
2020-11-27 19:57:31 +0530

    .

---
+++ build/precache-manifest.062c2bbd4e7d3c5a2f9bd29ca2b6f106.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebf996f49a2ea656cd37509408a4426f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c771b9572d40c7d9a8b",
+    "url": "/static/css/2.a09d231f.chunk.css"
+  },
+  {
+    "revision": "f8d14faea50447bd7b47",
+    "url": "/static/css/main.bc853de0.chunk.css"
+  },
+  {
+    "revision": "1c771b9572d40c7d9a8b",
+    "url": "/static/js/2.6009a2b5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6009a2b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8d14faea50447bd7b47",
+    "url": "/static/js/main.9735f6ab.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "b5366ff0c51d27a84f85796e5db36481",
+    "url": "/static/media/586.b5366ff0.jpg"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "dbffe7f53e8c8924a18462cdf6476f20",
+    "url": "/static/media/Room.dbffe7f5.jpg"
+  },
+  {
+    "revision": "e25561b3d3b54f14e6aa99da74c9319c",
+    "url": "/static/media/cutlery.e25561b3.png"
+  },
+  {
+    "revision": "cb1b940d52f1d59d7002553daca19d91",
+    "url": "/static/media/dislike.cb1b940d.png"
+  }
+]);
